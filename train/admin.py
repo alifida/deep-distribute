01dca3a8973bbb1e0186bb47f3a9e61b88ec1366
alifida <alifida.86@gmail.com>
2024-04-06 15:35:45 +0500

    Update admin.py

--- train/admin.py
+++ train/admin.py
@@ -9,7 +9,13 @@ class Dataset_IMGAdmin(ModelAdmin):
     search_fields=["status", "data_name", "user"]
     list_filter=["status", "data_name", "user"]
 
+class Training_jobAdmin (ModelAdmin):
 
+    list_display=["job_name", "dataset_img.data_name", "status", "algo", "user"]
+    search_fields=["job_name", "dataset_img.data_name", "status", "algo", "user"]
+    list_filter=["job_name", "dataset_img.data_name", "status", "algo", "user"]
+ 
  
 admin.site.register(models.Dataset_IMG)
+admin.site.register(models.Training_job)
  
