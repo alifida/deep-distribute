b8e8f1eb0f4c6ff0b95a60a2d5975b7dbf5743ce
alifida <alifida.86@gmail.com>
2024-05-11 16:42:56 +0500

    Data Path Check: Before starting training, the script checks if the dataset path exists. TensorBoard Setup: Proper initialization of TensorBoard within the TensorFlow strategy scope. Strategy and Cluster Resolver: Utilized ParameterServerStrategy with a SimpleClusterResolver for the defined cluster specification. Dataset Handling: Updated to initialize and compile the model inside the TensorFlow strategy scope.

--- train/services/TrainingService.py
+++ train/services/TrainingService.py
@@ -8,7 +8,6 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.callbacks import Callback
 import threading
-from datetime import datetime
 
 from train.dao.TrainingJobDAO import TrainingJobDAO
 from train.utils.JobStatus import JobStatus
@@ -22,94 +21,45 @@ class TrainingService:
 
     @staticmethod
     def start_training(job):
-        # Configure the strategy
-        node_type = os.getenv('NODE_TYPE', 'worker')  # Read from environment variable
-        index = int(os.getenv('NODE_INDEX', '0'))  # Read from environment variable
-        tf.debugging.set_log_device_placement(True) # TODO Remove in production
-        print("*********************TODO Remove in production************************")
-        print(" tf.debugging.set_log_device_placement(True)")
-        print("**********************************************************************")
-
-        print("*********************************************")
-        print("====================1")
-        
-        # Define the cluster specification
+        print("Configuring distributed strategy")
         cluster_spec = tf.train.ClusterSpec({
             "worker": ["192.168.10.106:2222", "192.168.10.71:2222"],
             "ps": ["192.168.10.106:2223"]
         })
-        print("====================2")
-        print()
-        # Create a cluster resolver and strategy
-        # strategy = tf.distribute.experimental.ParameterServerStrategy(
-        #     tf.distribute.cluster_resolver.SimpleClusterResolver(
-        #         cluster_spec=cluster_spec, rpc_layer="grpc"))
+        cluster_resolver = tf.distribute.cluster_resolver.SimpleClusterResolver(
+            cluster_spec=cluster_spec, rpc_layer="grpc"
+        )
+        strategy = tf.distribute.experimental.ParameterServerStrategy(cluster_resolver)
 
-        try:
-        # Create a cluster resolver and strategy
-            strategy = tf.distribute.experimental.ParameterServerStrategy(
-                tf.distribute.cluster_resolver.SimpleClusterResolver(
-                    cluster_spec=cluster_spec, rpc_layer="grpc"))
-        except Exception as e:
-            print("Error initializing ParameterServerStrategy:", e)
-            raise
-        print("====================3")
-        print()
         with strategy.scope():
             dataset_path = job.dataset_img.extracted_path
-            print('Dataset Path: ' + dataset_path)
-            print("====================4")
             if not os.path.exists(dataset_path):
                 print(f"Dataset path {dataset_path} does not exist.")
-                return  # Exit if the dataset path is invalid
+                return
 
-
-             # Setup TensorBoard logging
             log_dir = f"logs/fit/{datetime.now().strftime('%Y%m%d-%H%M%S')}"
             tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
-            print()
-            # Load the ResNet50 model pre-trained on ImageNet
+
             base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(150, 150, 3))
-            print("====================5")
-            print()
-            # Freeze the layers of the base model
             for layer in base_model.layers:
                 layer.trainable = False
 
-            # Add custom layers on top of ResNet50
             x = base_model.output
             x = GlobalAveragePooling2D()(x)
             x = Dense(1024, activation='relu')(x)
             predictions = Dense(1, activation='sigmoid')(x)
-
-            # Create the final model
             model = Model(inputs=base_model.input, outputs=predictions)
+            model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', 'Precision', 'Recall', 'MeanSquaredError'])
 
-            # Compile the model
-            model.compile(optimizer='adam', loss='binary_crossentropy',
-                          metrics=['accuracy', 'Precision', 'Recall', 'MeanSquaredError'])
-        print("====================6")
-        print()
-        # Set up your dataset
-        train_datagen = ImageDataGenerator(rescale=1./255)
-        train_generator = train_datagen.flow_from_directory(
-            dataset_path,
-            target_size=(150, 150),
-            batch_size=20,  # Adjust batch size to your needs
-            class_mode='binary')
-        print("====================7")
-        print()
-        terminate_on_flag_callback = TerminateOnFlagCallback(job.id)
+            train_datagen = ImageDataGenerator(rescale=1./255)
+            train_generator = train_datagen.flow_from_directory(dataset_path, target_size=(150, 150), batch_size=20, class_mode='binary')
 
-        # Train the model with the callback
-        model.fit(train_generator, epochs=10, callbacks=[tensorboard_callback, terminate_on_flag_callback])
-        print("====================8")
-        print()
-        # Update the job status in the database
-        training_job = TrainingJobDAO.get(job.id)
-        if training_job.status == JobStatus.RUNNING.value:
-            TrainingJobDAO.update(job.id, status=JobStatus.COMPLETED.value, ended_at=datetime.now())
+            terminate_on_flag_callback = TerminateOnFlagCallback(job.id)
+            model.fit(train_generator, epochs=10, callbacks=[tensorboard_callback, terminate_on_flag_callback])
 
+            training_job = TrainingJobDAO.get(job.id)
+            if training_job.status == JobStatus.RUNNING.value:
+                TrainingJobDAO.update(job.id, status=JobStatus.COMPLETED.value, ended_at=datetime.now())
 
 class TerminateOnFlagCallback(Callback):
     def __init__(self, job_id):
