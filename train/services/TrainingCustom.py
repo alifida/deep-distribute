2959e1e5a593505d29d11183dd0b784b8b4aac7b
alifida <alifida.86@gmail.com>
2025-02-19 22:00:20 +0500

    updates

--- train/services/TrainingCustom.py
+++ train/services/TrainingCustom.py
@@ -65,7 +65,7 @@ def set_dataset_details(dataset_id, init_params):
      
     dataset = DatasetImgDAO.get(dataset_id)
     init_params["dataset_details"] = dataset.gather_dataset_stats()
-    init_params["dataset_details"]["host_url"] = "http://localhost:89/media/tmp/"
+    init_params["dataset_details"]["host_url"] = settings.DATASET_HOST_URL
     return init_params
  
 
