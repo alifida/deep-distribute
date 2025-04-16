5fa86bebb3e7bfebbf3e6a3eaa986e17e075ddaf
Ali Fida <alifida.86@gmail.com>
2025-04-16 16:59:31 +0500

    Update settings.py

--- deepdistribute/settings.py
+++ deepdistribute/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*7o+es^8we7)hedbkteh0a5bw&0-0#w=z-alv^7$3(8mntm*!=
 DEBUG = True
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['localhost','gpu.predictive-lab.com','192.168.18.19','192.168.35.227', '192.168.10.120', '192.168.10.106', '192.168.10.71','192.168.100.109','192.168.100.112', '192.168.10.12', '192.168.10.92', '192.168.137.80']
+ALLOWED_HOSTS = ['localhost','gpu.predictive-lab.com','192.168.10.55','192.168.18.19','192.168.35.227', '192.168.10.120', '192.168.10.106', '192.168.10.71','192.168.100.109','192.168.100.112', '192.168.10.12', '192.168.10.92', '192.168.137.80']
 CLUSTER_DETAIL_URL = 'http://192.168.10.120:89/api/cluster/detail'
 SHARED_DIRECTORY = 'E:\\dataset\\training_tmp'
 
